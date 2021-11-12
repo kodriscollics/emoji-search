fb972ba0beeaed69eb564174cd0a36ce2b01927d
Kent O'Driscoll <75755773+kodriscollics@users.noreply.github.com>
2021-11-12 13:24:12 -0700

    Added Yum

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -4,5 +4,9 @@ describe('Sample test', () => {
         cy.get('input').type('lollipop') // enter 'lollipop' in search 
         cy.get('.component-emoji-result-row').should('have.length', 1) // check if only 1 result is returned 
         cy.contains('Lollipop').should('have.class', 'title') // check if returned element contains 'title' class
+		cy.get('input').clear().type('Yum')
+		cy.get('.component-emoji-result-row').should('have.length', 1)
+        cy.contains('Yum').should('have.class', 'title')
+
     })
 })
